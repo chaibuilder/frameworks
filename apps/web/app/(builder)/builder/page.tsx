23d830c2e2d98714accd1aae6fa1171d5c5db618
Suraj Air <surajair@gmail.com>
2025-07-23 14:35:44 +0530

    feat: implement builder API handler with auth and revalidation support

--- apps/web/app/(builder)/builder/page.tsx
+++ apps/web/app/(builder)/builder/page.tsx
@@ -4,5 +4,11 @@ import ChaiBuilder from "chai-next";
 import "chai-next/builder-styles";
 
 export default function Page() {
-  return <ChaiBuilder />;
+  return (
+    <ChaiBuilder
+      apiUrl={"/builder/api"}
+      usersApiUrl={"/builder/api"}
+      assetsApiUrl={"/builder/api"}
+    />
+  );
 }
--- packages/next/src/server/builder-api-handler.ts
+++ packages/next/src/server/builder-api-handler.ts
@@ -1,10 +1,55 @@
+import {
+  ChaiBuilderPages,
+  ChaiBuilderPagesBackend,
+} from "@chaibuilder/pages/server";
+import { get, has } from "lodash";
+import { revalidateTag } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
+
 export const builderApiHandler = (apiKey: string) => {
-  return () => {
-    return {
-      status: 200,
-      body: {
-        apiKey,
-      },
-    };
+  const chaiBuilderPages = new ChaiBuilderPages(
+    new ChaiBuilderPagesBackend(apiKey)
+  );
+  return async (req: NextRequest) => {
+    try {
+      const requestBody = await req.json();
+      // Check for `authorization` header
+      const authorization = req.headers.get("authorization");
+      if (!authorization) {
+        return NextResponse.json(
+          { error: "Missing Authorization header" },
+          { status: 401 }
+        );
+      }
+
+      // Check and extract, valid token string `authorization`
+      const authToken = (
+        authorization ? authorization.split(" ")[1] : ""
+      ) as string;
+      const response = await chaiBuilderPages.handle(requestBody, authToken);
+
+      const tags = get(response, "tags", []);
+      for (const tag of tags) {
+        revalidateTag(tag);
+      }
+
+      if (has(response, "error")) {
+        return NextResponse.json(response, { status: response.status });
+      }
+      return NextResponse.json(response);
+    } catch (error) {
+      // * On error, throw if firebase auth error, else 500
+      if (error instanceof Error) {
+        return NextResponse.json(
+          { error: "Invalid or expired token" },
+          { status: 401 }
+        );
+      } else {
+        return NextResponse.json(
+          { error: "Something went wrong." },
+          { status: 500 }
+        );
+      }
+    }
   };
 };

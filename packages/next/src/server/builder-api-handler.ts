cd47b53d5482f5f9588eeb59429f939b80cbc23b
Suraj Air <surajair@gmail.com>
2025-08-14 22:14:32 +0530

    feat: update package version to 0.0.3-beta and upgrade dependencies; enhance class definitions and refactor API handler for improved readability

--- packages/next/src/server/builder-api-handler.ts
+++ packages/next/src/server/builder-api-handler.ts
@@ -1,17 +1,12 @@
-import {
-  ChaiBuilderPages,
-  ChaiBuilderPagesBackend,
-} from "@chaibuilder/pages/server";
+import { ChaiBuilderPages, ChaiBuilderPagesBackend } from "@chaibuilder/pages/server";
 import { get, has } from "lodash";
-// import { revalidateTag } from "next/cache";
+import { revalidateTag } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 
 const BYPASS_AUTH_CHECK_ACTIONS = ["LOGIN"];
 
 export const builderApiHandler = (apiKey: string) => {
-  const chaiBuilderPages = new ChaiBuilderPages(
-    new ChaiBuilderPagesBackend(apiKey)
-  );
+  const chaiBuilderPages = new ChaiBuilderPages(new ChaiBuilderPagesBackend(apiKey));
   return async (req: NextRequest) => {
     try {
       const requestBody = await req.json();
@@ -19,16 +14,11 @@ export const builderApiHandler = (apiKey: string) => {
       // Check for `authorization` header
       const authorization = req.headers.get("authorization");
       if (checkAuth && !authorization) {
-        return NextResponse.json(
-          { error: "Missing Authorization header" },
-          { status: 401 }
-        );
+        return NextResponse.json({ error: "Missing Authorization header" }, { status: 401 });
       }
 
       // Check and extract, valid token string `authorization`
-      const authToken = (
-        authorization ? authorization.split(" ")[1] : ""
-      ) as string;
+      const authToken = (authorization ? authorization.split(" ")[1] : "") as string;
       const response = await chaiBuilderPages.handle(requestBody, authToken);
 
       if (has(response, "error")) {
@@ -37,21 +27,15 @@ export const builderApiHandler = (apiKey: string) => {
 
       const tags = get(response, "tags", []);
       for (const tag of tags) {
-        // revalidateTag(tag);
+        revalidateTag(tag);
       }
       return NextResponse.json(response);
     } catch (error) {
       // * On error, throw if firebase auth error, else 500
       if (error instanceof Error) {
-        return NextResponse.json(
-          { error: "Invalid or expired token" },
-          { status: 401 }
-        );
+        return NextResponse.json({ error: "Invalid or expired token" }, { status: 401 });
       } else {
-        return NextResponse.json(
-          { error: "Something went wrong." },
-          { status: 500 }
-        );
+        return NextResponse.json({ error: "Something went wrong." }, { status: 500 });
       }
     }
   };

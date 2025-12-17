e5708e10b0fb2a143963c984ba1eb267e598dbea
amitdhiman5086 <amitdhiman5086@gmail.com>
2025-12-17 10:10:04 +0530

    remove: delete dev page and update package dependencies

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,11 +1,13 @@
 import { withChaiBuilder } from "chai-next/config";
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
+  turbopack: {},
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "ucarecdn.com" },
       { protocol: "https", hostname: "via.placeholder.com" },
     ],
   },
+  serverExternalPackages: ["canvas", "konva", "shiki"],
 };
 export default withChaiBuilder(nextConfig) as NextConfig;

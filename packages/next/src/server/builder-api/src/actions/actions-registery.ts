89be91ac86e7ad4725d2c9f015cf11b4762b8cea
amitdhiman5086 <amitdhiman5086@gmail.com>
2026-01-05 15:59:23 +0530

    feat: register GetTemplatesByTypeAction in actions registry

--- packages/next/src/server/builder-api/src/actions/actions-registery.ts
+++ packages/next/src/server/builder-api/src/actions/actions-registery.ts
@@ -17,6 +17,7 @@ import { GetWebsitePagesAction } from "./get-website-pages";
 import { GetWebsiteSettingsAction } from "./get-website-settings";
 import { UpdatePageAction } from "./update-page";
 import { UpdatePageMetadataAction } from "./update-page-metadata";
+import { GetTemplatesByTypeAction } from "./get-templates-by-type";
 
 /**
  * Registry of all available actions
@@ -45,6 +46,7 @@ class ActionsRegistry {
     this.register("GET_LIBRARIES", new GetLibrariesAction());
     this.register("GET_DRAFT_PAGE", new GetDraftPageAction());
     this.register("GET_LANGUAGE_PAGES", new GetLanguagePagesAction());
+    this.register("GET_TEMPLATES_BY_TYPE", new GetTemplatesByTypeAction());
     // Add more actions here as they are created
   }
 

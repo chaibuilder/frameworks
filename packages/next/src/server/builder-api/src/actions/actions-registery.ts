931a723a5940db2298701247111fdd2e786c9dd4
amitdhiman5086 <amitdhiman5086@gmail.com>
2026-01-05 15:54:50 +0530

    feat: add SearchPagesAction to actions registry

--- packages/next/src/server/builder-api/src/actions/actions-registery.ts
+++ packages/next/src/server/builder-api/src/actions/actions-registery.ts
@@ -17,6 +17,7 @@ import { GetWebsitePagesAction } from "./get-website-pages";
 import { GetWebsiteSettingsAction } from "./get-website-settings";
 import { UpdatePageAction } from "./update-page";
 import { UpdatePageMetadataAction } from "./update-page-metadata";
+import { SearchPagesAction } from "./search-pages";
 
 /**
  * Registry of all available actions
@@ -45,6 +46,7 @@ class ActionsRegistry {
     this.register("GET_LIBRARIES", new GetLibrariesAction());
     this.register("GET_DRAFT_PAGE", new GetDraftPageAction());
     this.register("GET_LANGUAGE_PAGES", new GetLanguagePagesAction());
+    this.register("SEARCH_PAGES", new SearchPagesAction());
     // Add more actions here as they are created
   }
 
